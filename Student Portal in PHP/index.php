46fbe86024aa16026a29ff94f150c00bee5daa43
Ayesha Noreen <154872130+KhatoonInTech@users.noreply.github.com>
2024-11-08 22:45:31 +0500

    Delete Student Portal in PHP directory

--- Student Portal in PHP/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-       
-        session_start();
-        header("Location: dashbord.php");
-        exit();
-?>
--- Student Portal in PHP/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start(); // Start the session
-    // Destroy the session.
-    session_destroy();
-    // Redirect to registration page
-    header("Location: dashbord.php");
-    exit();
-?>
